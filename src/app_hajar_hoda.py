86ffcb0db64877cce57eab73f5a64f96f06b89c1
Mahnaz Ghassemi <mahnaz.ghssm@gmail.com>
2026-07-21 15:43:04 +0330

    omit .ipynb

--- src/app_hajar_hoda.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from test import response 
-st.title(":dollar: Currency Converter")
-st.markdown(""" This tool allows you to instantly converter amounts between different currencies. Enter the amount and choose the currencies to see the result.""")
-url = f"https: //api.exchangerate-api.com/v4/latest/USD"
-import requests
-response = requests.get(url)
-list response.json(["rates"].keys())
